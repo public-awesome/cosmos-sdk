a174f8199e8e7e21b06f3ced91ec1f01ef3357f0
Tasio Victoria <tasiovictoria@gmail.com>
2023-10-17 22:32:01 -0400

    Add state snapshot export functionality

--- baseapp/abci.go
+++ baseapp/abci.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"sort"
 	"strings"
 	"syscall"
@@ -415,6 +417,57 @@ func (app *BaseApp) snapshot(height int64) {
 
 		app.logger.Debug("pruned state snapshots", "pruned", pruned)
 	}
+
+	if os.Getenv("SNAPSHOT_EXPORT_ENABLED") == "1" {
+		app.logger.Info("exporting state snapshot", "height", height)
+		if err := app.ExportSnapshot(snapshot); err != nil {
+			app.logger.Error("failed to export snapshot", "height", height, "err", err)
+		}
+	}
+}
+
+func (app *BaseApp) ExportSnapshot(snapshot *snapshottypes.Snapshot) error {
+	snapshotsDir := os.Getenv("SNAPSHOT_EXPORT_DIR")
+	if snapshotsDir == "" {
+		return errors.New("SNAPSHOT_EXPORT_DIR env var is not set, not exporting the snapshot")
+	}
+
+	baseDir := fmt.Sprintf("%s/%d", snapshotsDir, snapshot.Height)
+	snapshotPath := fmt.Sprintf("%s/snapshot", baseDir)
+	chunkPath := filepath.Join(baseDir, "%d.chunk")
+
+	if err := os.MkdirAll(baseDir, 0755); err != nil {
+		return err
+	}
+
+	abciSnap, err := snapshot.ToABCI()
+	if err != nil {
+		return err
+	}
+
+	data, err := abciSnap.Marshal()
+	if err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(snapshotPath, data, 0666); err != nil {
+		return err
+	}
+
+	for i := uint32(0); i < snapshot.Chunks; i++ {
+		app.logger.Info("exporting snapshot chunk", "height", snapshot.Height, "chunk", i)
+
+		chunkData, err := app.snapshotManager.LoadChunk(snapshot.Height, snapshot.Format, i)
+		if err != nil {
+			return err
+		}
+
+		if err := ioutil.WriteFile(fmt.Sprintf(chunkPath, i), chunkData, 0666); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // Query implements the ABCI interface. It delegates to CommitMultiStore if it
